dac4b63215fec421c6ad3a99ebfa112ba00299c0
Geert van Geest <geert.vangeest@bioinformatics.unibe.ch>
2021-08-24 08:13:03 +0200

    symbol field as option

--- FindPeaks.R
+++ FindPeaks.R
@@ -6,7 +6,8 @@ option_list <- list(
     make_option(c("-b", "--bam"), type="character", help="Input BAM"),
     make_option(c("-j", "--junctions"), type="character", help="junctions BED file"),
     make_option(c("-o", "--output"), type = "character", default = "peaks.txt", help = "Output file [default = %default]"),
-    make_option(c("-c", "--ncores"), type = "integer", default = 1, help = "Number of cores[default= %default]")
+    make_option(c("-c", "--ncores"), type = "integer", default = 1, help = "Number of cores[default= %default]"),
+    make_option(c("-s", "--symbol_field", type = "character", default = "gene_name", help = "field in the GTF file containing the gene symbol"))
 )
  
 opt_parser <- OptionParser(option_list=option_list)
@@ -19,4 +20,5 @@ FindPeaks(output.file = opt$output,   # output filename
           gtf.file = opt$gtf,           # gene model as a GTF file
           bamfile = opt$bam,                # BAM alignment filename.
           junctions.file = opt$junctions,     # BED filename of splice junctions exising in BAM file. 
-          ncores = opt$ncores)                          # number of cores to use
+          ncores = opt$ncores,
+          gene.symbol.ref = opt$symbol_field)                          # number of cores to use
