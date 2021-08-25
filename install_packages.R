dc8b8909d57b64936a27b689378eb7919e470787
Geert van Geest <geert.vangeest@bioinformatics.unibe.ch>
2021-08-25 10:46:12 +0200

    only base installation Sierra

--- install_packages.R
+++ install_packages.R
@@ -3,4 +3,4 @@ install.packages(c("optparse", "devtools", "BiocManager"))
 BiocManager::install(c("Rsamtools", "GenomicAlignments", "Gviz", "DEXSeq", "BSgenome", "GenomicFeatures", "rtracklayer"))
 
 # BiocManager::install("GenomicAlignments", "SingleCellExperiment", "Gviz", "DEXSeq", )
-devtools::install_github("geertvangeest/Sierra", build = TRUE, build_vignettes = FALSE, dependencies = TRUE, build_opts = c("--no-resave-data", "--no-manual"))
+devtools::install_github("geertvangeest/Sierra", build = TRUE)
